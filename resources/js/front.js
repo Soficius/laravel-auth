c851ffd22847ce6bdfea44dd9b65f3bc32294d21
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-26 21:39:52 +0200

    added front js and vue

--- resources/js/front.js
+++ resources/js/front.js
@@ -5,3 +5,11 @@
  */
 
 require('./bootstrap');
+window.Vue = required('vue');
+
+import App from './components/App.vue'
+
+const root = new Vue({
+    el: '#root',
+    render: h => h(App)
+});
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -9,7 +9,8 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+    {{-- script --}}
+    <script defer src="{{ asset('js/front.js') }}"></script>
     <!-- Styles -->
     <style>
         html,
@@ -82,23 +83,10 @@
             </div>
         @endif
 
-        <div class="content">
-            <div class="title m-b-md">
-                Laravel
-            </div>
+        <div id="root"></div>
 
-            <div class="links">
-                <a href="https://laravel.com/docs">Docs</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://blog.laravel.com">Blog</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://vapor.laravel.com">Vapor</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-            </div>
-        </div>
     </div>
+
 </body>
 
 </html>
