aa2cf3694a5a7a7f6c69620e374f78c02e08eb34
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-27 17:39:03 +0200

    add PostSeeder and create posts index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,5 +2,34 @@
 @section('content')
     <header>
         <h1>Posts</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Autore</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Creato il</th>
+                    <th scope="col">Modificato il</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->slug }}</td>
+                        <td>{{ $post->created_at }}</td>
+                        <td>{{ $post->updated_at }}</td>
+                        <td></td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6">
+                            <h4 class="text-center">Nessun Post</h4>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
     </header>
 @endsection
