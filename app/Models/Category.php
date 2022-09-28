90de4bd21fb6b66fbc39884dff047bc95ad153b7
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-28 17:06:51 +0200

    created FK and added funcions in models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //creiamo la relazione con post
+    public function posts()
+    {
+        // utilizziamo il verbp hasMany perchè category è l'istanza forte
+        return $this->hasMany('App\Models\Post');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'image', 'content', 'slug'];
+    // creiamo la relazione con category
+    public function category()
+    {
+        // il verbo che usiamo è belong perche post è l'istanza debole
+        return $this->belongsTo('App\Models\Category');
+    }
 }
